b798a5fc1a1bb38eeed9fa5df284dbba580e59f4
Afrifood-Paul <docpaul2016@gmail.com>
2024-12-25 13:39:09 +0100

    update

--- src/routeHelper/Settings/ProfileSection.tsx
+++ src/routeHelper/Settings/ProfileSection.tsx
@@ -1,23 +1,16 @@
 // ProfileSection.tsx
 interface ProfileSectionProps {
     profileImage: string;
-    name: string;
-    email: string;
-    phone: string;
-    username: string;
     onEdit: () => void;
   }
   
   const ProfileSection: React.FC<ProfileSectionProps> = ({
     profileImage,
-    name,
-    email,
-    phone,
-    username,
+   
     onEdit
   }) => {
     return (
-      <div className="w-[70%]">
+      <div className="w-[70%] lg:mx-16 my-5">
         <div className="relative w-52 rounded-lg border border-[#e5f4fc]">
           <div className="UserImg">
             <img
@@ -36,22 +29,7 @@ interface ProfileSectionProps {
   
         <div className="lg:w-[70%] xxl:w-[50%] py-10">
           <div className="grid smd:grid-cols-2 gap-5">
-            <div className="xl:pb-10">
-              <h2 className="smd:text-2xl lg:text-lg font-medium">{name}</h2>
-              <p className="smd:text-lg lg:text-sm text-gray-500">Full Name:</p>
-            </div>
-            <div>
-              <h2 className="smd:text-2xl lg:text-lg font-medium">{username}</h2>
-              <p className="smd:text-lg lg:text-sm text-gray-500">Username:</p>
-            </div>
-            <div>
-              <h2 className="smd:text-2xl lg:text-lg font-medium">{email}</h2>
-              <p className="smd:text-lg lg:text-sm text-gray-500">Email Address:</p>
-            </div>
-            <div>
-              <p className="smd:text-2xl lg:text-lg font-medium">{phone}</p>
-              <p className="smd:text-lg lg:text-sm text-gray-500">Phone Number:</p>
-            </div>
+            
             <div className="flex flex-col gap-10 text-lg">
               <button onClick={onEdit} className="smd:text-2xl lg:text-lg text-gray-500">
                 Edit Profile
--- src/routes/_auth/setting.tsx
+++ src/routes/_auth/setting.tsx
@@ -1,42 +1,38 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { useState } from 'react'
+import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import ProfileImage from "../../routeHelper/Settings/assets/userImg.svg";
+import PasswordModal from "../../components/modal/PasswordModal";
 
-import PasswordModal from '../../components/modal/PasswordModal'
+import ProfileSection from "../../routeHelper/Settings/ProfileSection";
+// import Modal from '../../components/modal/modal'
 
-import ProfileSection from '../../routeHelper/Settings/ProfileSection'
-import Modal from '../../components/modal/modal'
-
-export const Route = createFileRoute('/_auth/setting')({
+export const Route = createFileRoute("/_auth/setting")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
-  const [isModalPassword, setIsModalPassword] = useState<boolean>(false)
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [isModalPassword, setIsModalPassword] = useState<boolean>(false);
 
   const toggleModalVisible = () => {
-    setIsModalVisible(!isModalVisible)
-  }
+    setIsModalVisible(!isModalVisible);
+  };
 
   const toggleModalPassword = () => {
-    setIsModalPassword(!isModalPassword)
-  }
+    setIsModalPassword(!isModalPassword);
+  };
 
   return (
     <div className="relative w-full">
       <div className="lg:w-full">
-        <div className="w-[85%] mx-auto mt-14 smd:mt-14 lg:mt-12 xl:mt-0">
-          <h1 className="text-[34px] py-10 xl:py-5 text-[#1D1D1D] leading-[28px] font-semibold">
+        <div className="mx-auto mt-14 w-[90%] smd:mt-14 lg:mt-12 xl:mt-0">
+          <h1 className="py-10 text-[34px] font-semibold leading-[28px] text-[#1D1D1D] xl:py-5">
             Settings
           </h1>
         </div>
 
         <ProfileSection
-          profileImage="ProfileImage.png" // Replace with actual image path
-          name="Chinedu Oke"
-          email="ChineduOke@gmail.com"
-          phone="08032324355"
-          username="ChineduOke"
+          profileImage={ProfileImage}
           onEdit={toggleModalVisible}
         />
 
@@ -50,7 +46,5 @@ function RouteComponent() {
         <Modal/> */}
       </div>
     </div>
-  )
+  );
 }
-
-
