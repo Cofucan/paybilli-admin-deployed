3552d0097a6e9b1cdb85331942f24784edc4ab48
Afrifood-Paul <docpaul2016@gmail.com>
2024-12-25 14:31:38 +0100

    settings updated

--- src/routeHelper/Settings/ProfileSection.tsx
+++ src/routeHelper/Settings/ProfileSection.tsx
@@ -1,48 +1,69 @@
-// ProfileSection.tsx
+import React from "react";
+
 interface ProfileSectionProps {
-    profileImage: string;
-    onEdit: () => void;
-  }
-  
-  const ProfileSection: React.FC<ProfileSectionProps> = ({
-    profileImage,
-   
-    onEdit
-  }) => {
-    return (
-      <div className="w-[70%] lg:mx-16 my-5">
+  EditPen: string;
+  profileImage: string;
+  onEdit: () => void;
+  onEditPassword: () => void;
+  onViewLogs: () => void;
+}
+
+const ProfileSection: React.FC<ProfileSectionProps> = ({
+  profileImage,
+  EditPen,
+  onEdit,
+  onEditPassword,
+  onViewLogs,
+}) => {
+  return (
+    <div className="mx-auto my-5 flex w-[85%] flex-col items-start justify-between gap-10 lg:flex-row lg:items-center">
+      {/* Profile Image Section */}
+      <div className="flex flex-col items-center">
         <div className="relative w-52 rounded-lg border border-[#e5f4fc]">
           <div className="UserImg">
             <img
               src={profileImage}
-              alt="userpics"
-              className="w-full h-full object-cover"
+              alt="User Profile"
+              className="h-full w-full rounded-lg object-cover"
             />
           </div>
-          <div className="EditUserPics absolute bottom-20 -right-5">
-            {/* <img src={EditProfile} alt="EditProfile" /> */}
+          <div className="EditUserPics absolute -right-5 bottom-20">
+            {" "}
+            <img src={EditPen} alt="EditProfile" />
           </div>
-          <div className="text-center py-2 text-gray-500">
-            <p>Admin</p>
+          <div className="py-4 text-center">
+            <p className="text-gray-500">Admin</p>
           </div>
         </div>
-  
-        <div className="lg:w-[70%] xxl:w-[50%] py-10">
-          <div className="grid smd:grid-cols-2 gap-5">
-            
-            <div className="flex flex-col gap-10 text-lg">
-              <button onClick={onEdit} className="smd:text-2xl lg:text-lg text-gray-500">
-                Edit Profile
-              </button>
-              <button className="smd:text-2xl lg:text-lg text-gray-500">
-                View Activity Logs
-              </button>
-            </div>
-          </div>
+
+        {/* Action Buttons Below Image */}
+        <div className="mt-6 flex flex-col gap-4">
+          <button
+            onClick={onEditPassword}
+            className="w-52 rounded-md px-6 py-3 text-lg text-gray-500"
+          >
+            Change Password
+          </button>
+          <button
+            onClick={onViewLogs}
+            className="w-52 rounded-md px-6 py-3 text-lg text-gray-500"
+          >
+            View Activity Logs
+          </button>
         </div>
       </div>
-    );
-  };
-  
-  export default ProfileSection;
-  
+
+      {/* Edit Profile Button on the Right */}
+      <div className="hidden lg:-mt-[17rem] lg:block">
+        <button
+          onClick={onEdit}
+          className="rounded-md bg-light-blue-500 px-14 py-4 text-sm font-medium text-white"
+        >
+          Edit Profile
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileSection;
--- src/routes/_auth/setting.tsx
+++ src/routes/_auth/setting.tsx
@@ -1,10 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import ProfileImage from "../../routeHelper/Settings/assets/userImg.svg";
+import EditPen from "../../routeHelper/Settings/assets/editPen.svg";
 import PasswordModal from "../../components/modal/PasswordModal";
-
 import ProfileSection from "../../routeHelper/Settings/ProfileSection";
-// import Modal from '../../components/modal/modal'
+import Modal from "../../components/modal/modal";
 
 export const Route = createFileRoute("/_auth/setting")({
   component: RouteComponent,
@@ -25,7 +25,7 @@ function RouteComponent() {
   return (
     <div className="relative w-full">
       <div className="lg:w-full">
-        <div className="mx-auto mt-14 w-[90%] smd:mt-14 lg:mt-12 xl:mt-0">
+        <div className="mx-auto mt-14 w-[85%] smd:mt-14 lg:mt-12 xl:mt-3">
           <h1 className="py-10 text-[34px] font-semibold leading-[28px] text-[#1D1D1D] xl:py-5">
             Settings
           </h1>
@@ -34,6 +34,9 @@ function RouteComponent() {
         <ProfileSection
           profileImage={ProfileImage}
           onEdit={toggleModalVisible}
+          onEditPassword={toggleModalPassword}
+          onViewLogs={toggleModalVisible}
+          EditPen={EditPen}
         />
 
         <PasswordModal
@@ -41,9 +44,14 @@ function RouteComponent() {
           toggleVisibility={toggleModalPassword}
         />
 
-        {/* < Modal isVisible={isModalVisible} toggleVisibility={toggleModalVisible} title="Edit Profile">
-          Add the form content for editing profile
-        <Modal/> */}
+        <Modal
+          isVisible={isModalVisible}
+          toggleVisibility={toggleModalVisible}
+          title="Edit Profile"
+          children="children"
+        >
+          {/* Add the form content for editing profile */}
+        </Modal>
       </div>
     </div>
   );
